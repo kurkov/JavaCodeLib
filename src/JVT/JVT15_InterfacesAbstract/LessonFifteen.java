22bac1d1d58cc561e819124a7c19d23e34a8fade
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-05-04 21:31:43 +0300

    Initial commit

---
+++ src/JVT/JVT15_InterfacesAbstract/LessonFifteen.java
@@ -0,0 +1,19 @@
+package JVT.JVT15_InterfacesAbstract;
+
+public class LessonFifteen {
+
+    public static void main(String[] args) {
+
+        Vehicle car = new Vehicle(4, 100.0);
+
+        // Using methods from the interface Drivable
+        System.out.println("Cars Max Speed: " + car.getSpeed());
+        System.out.println("Cars Number of Wheels: " + car.getWheels());
+
+        // Using methods from abstract method Crashable
+        car.setCarStrength(10);
+        System.out.println("Car Strength: " + car.getCarStrength());
+
+    }
+
+}
